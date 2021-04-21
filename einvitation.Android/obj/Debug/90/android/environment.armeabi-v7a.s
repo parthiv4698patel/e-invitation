b5ba6f4812659901727e53c57bd50e69978e1e08
parthiv4698patel <78160490+parthiv4698patel@users.noreply.github.com>
2021-04-21 15:36:49 +0530

    Delete Operation completed
    I completed delete operation with delete popup.

--- einvitation.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ einvitation.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"b28cce1e-878e-4d6e-ac0e-8f833f946995"
+	.asciz	"70f92547-514a-4fdd-9aef-dc45638ba534"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
