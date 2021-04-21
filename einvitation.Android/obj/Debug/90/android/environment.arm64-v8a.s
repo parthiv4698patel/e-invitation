8b8f0f0fe37f6769520da7d8a7f00f057f3f95c7
parthiv4698patel <78160490+parthiv4698patel@users.noreply.github.com>
2021-04-21 15:46:11 +0530

    completed data store, display and delete
    I completed data storage using sqlite, display data in main dashboard and delete selected data from the list of events.

--- einvitation.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ einvitation.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"70f92547-514a-4fdd-9aef-dc45638ba534"
+	.asciz	"2a344d02-97c5-4fa7-92dc-11291e2b90fd"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
